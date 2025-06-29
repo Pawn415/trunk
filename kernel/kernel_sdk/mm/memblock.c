99d9fe78c6592d25d480afd1946532dc359dce26
何庆龙 <1836330111@qq.com>
2025-06-29 10:23:55 +0800

    ftrace配置修改

--- kernel/kernel_sdk/mm/memblock.c
+++ kernel/kernel_sdk/mm/memblock.c
@@ -50,7 +50,7 @@ struct memblock memblock __initdata_memblock = {
 	.current_limit		= MEMBLOCK_ALLOC_ANYWHERE,
 };
 
-int memblock_debug =1;
+int memblock_debug =0;
 #ifdef CONFIG_MOVABLE_NODE
 bool movable_node_enabled __initdata_memblock = false;
 #endif
